9124f27f334adbc1b99cc865def84c86778fc76d
divyaa191 <divyarxl1908@gmail.com>
2024-03-02 15:07:07 +0530

    init2

--- basic.c
+++ basic.c
@@ -7,6 +7,7 @@ int main()
     int point1,point2,point3,point4,point5,point6,point7,point8,point9,point10;
     int point01,point02,point03,point04,point05,point06,point07,point08,point09,point010;
     int total1,total2,total3;
+    printf("Amrit and Harsh are best friends");
 
     printf("Welcome to the Game\n\n");
 
